1207a7751a8e8ce9de1cbdb4b741421a6cb6b8c8
Ivan Nikitenko <ivan.nikitenko@gmail.com>
2025-05-08 11:09:16 +0100

    Create a NetflixDB class for testing and pass it to the test function

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import sqlite3
 from modules.api.clients.github import GitHub 
 
 
@@ -21,9 +22,7 @@ class User:
 def user():
     user = User()
     user.create()
-    
     yield user
-
     user.remove()
 
 
@@ -32,3 +31,22 @@ def github_api():
     api = GitHub()
     yield api
 
+# Create a NetflixDB class for testing and pass it to the test function
+# Створюю клас NetflixDB для тестування і передаю його тестовій функції
+class NetflixDB:
+    def __init__(self, db_path='netflixdb.sqlite'):
+        self.connection = sqlite3.connect(db_path)
+        self.cursor = self.connection.cursor()
+
+    def execute_query(self, query):
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def close(self):
+        self.connection.close()
+
+@pytest.fixture(scope="module")
+def netflix_db():
+    db = NetflixDB()
+    yield db
+    db.close()
